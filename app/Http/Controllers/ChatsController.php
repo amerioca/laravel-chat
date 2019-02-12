66bb7f69c9701307cf13f511ea6493e85be28d35
Derek Graetz <dgraetz@gmail.com>
2019-02-12 16:37:12 -0200

    first commit

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -2,59 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use App\Message;
 use App\Events\MessageSent;
+use App\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class ChatsController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+class ChatsController extends Controller {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware('auth');
+	}
 
-    /**
-     * Show chats
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('chat');
-    }
+	/**
+	 * Show chats
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		return view('chat');
+	}
 
-    /**
-     * Fetch all messages
-     *
-     * @return Message
-     */
-    public function fetchMessages()
-    {
-        return Message::with('user')->get();
-    }
+	/**
+	 * Fetch all messages
+	 *
+	 * @return Message
+	 */
+	public function fetchMessages() {
+		return Message::with('user')->get();
+	}
 
-    /**
-     * Persist message to database
-     *
-     * @param  Request $request
-     * @return Response
-     */
-    public function sendMessage(Request $request)
-    {
-        $user = Auth::user();
+	/**
+	 * Persist message to database
+	 *
+	 * @param  Request $request
+	 * @return Response
+	 */
+	public function sendMessage(Request $request) {
+		$user = Auth::user();
 
-        $message = $user->messages()->create([
-            'message' => $request->input('message')
-        ]);
+		$message = $user->messages()->create([
+			'message' => $request->input('message'),
+		]);
 
-        broadcast(new MessageSent($user, $message))->toOthers();
+		broadcast(new MessageSent($user, $message))->toOthers();
 
-        return ['status' => 'Message Sent!'];
-    }
+		return ['status' => 'Message Sent!'];
+	}
 }
